3a9b2d0e668e3a660f853df16b454ef2d24a8f47
aeronautryan <rpw218@lehigh.edu>
2015-01-23 16:32:03 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Ryan Wheeler
+//Homework 1
+//Welcome Class
+
+//Create Class
+
+public class WelcomeClass{
+    
+    //Create Main
+    
+    public static void main (String[] args){
+        
+        //Create the print statements
+        //The double slash in print statement is due to \ being an escape statement
+        
+        System.out.println("   ---------------    ");
+        System.out.println("   |   WELCOME   |    ");
+        System.out.println("   ---------------    ");
+        System.out.println("   ^  ^  ^  ^  ^  ^ ");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println(" <-R--P--W--2--1--8-> ");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("   v  v  v  v  v  v  ");
+        System.out.println("This winter break I got to enjoy a vacation to ");
+        System.out.println("Morocco, where I visited my sister who is studying ");
+        System.out.println("abroad.");
+        
+    }
+    
+}
