c2b3ed4afa43637e3729de33981a50b596d1e9d3
aeronautryan <rpw218@lehigh.edu>
2015-01-23 15:43:08 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//Ryan Wheeler
+//Lab01 - Hello World
+//Compile javac HelloWorld.java
+//Run java HelloWorld//
+
+//Adds a class
+public class HelloWorld{
+    
+    //Add a main
+    public static void main(String[]args) {
+        
+        //Print out statement using System.out.print, not return.
+        System.out.println("\t#########################");
+        System.out.println("\t#########################");
+        System.out.println("\t#                       #");
+        System.out.println("\t#   Hello World 2015!   #");
+        System.out.println("\t#                       #");
+        System.out.println("\t#########################");
+        System.out.println("\t#########################");
+        System.out.println("\ttest");
+        
+    } 
+    
+}
